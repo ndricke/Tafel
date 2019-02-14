4806c4fc165eaba21307e3153175592ad4165d3e
ndricke <nathan.d.ricke@gmail.com>
2019-02-14 10:39:27 -0500

    Quick save

--- data_fit/fit_goldHER.py
+++ data_fit/fit_goldHER.py
@@ -78,7 +78,7 @@ class RevPcet2(tafel.ElecMech.ElecMech):
         rate = 0.
         mod_ddGs = self.ddGs*0.5*sig*2**0.5
         for i in range(len(self.weights)):
-            self.genConsts((self.dG, dG1+mod_ddGs[i], T1, T2))
+            self.genConsts((dG1+mod_ddGs[i], T1, T2))
             rate += self.weights[i]*self.rate(VpH[0,:], VpH[1,:])
         return np.log10(rate)
 
@@ -104,8 +104,33 @@ class RevPcetEt(RevPcet2):
         k2, kn2 = self.k2*np.exp(-0.5*self.f*V), self.kn2*np.exp(0.5*self.f*V)
         return (k1*k2 - kn1*kn2)/(k1 + k2 + kn1 + kn2)
 
+"""Mechanism with a PCET followed by split proton and electron transfer"""
+class Rev2PTET(RevPcet2):
 
+    def rate(s, V, pH):
+        H = 10.**(-pH)
+        k1, kn1 = s.PCET((s.k1,s.kn1), V=V, H=H, mech='acid')
+        k2, kn2 = s.PT((s.k2,s.kn2), H=H, mech='acid')
+        k3, kn3 = s.ET((s.k3,s.kn3), V=V)
+        return (k1*k2*k3 - kn1*kn2*kn3)/(k1*k2 + k1*k3 + k2*k3 + k3*kn1 + k1*kn2 + kn1*kn2 + k2*kn3 + kn1*kn3 + kn2*kn3)
 
+    def genConsts(self, dGi_Ti):
+        self.k1, self.kn1 = self.gen_k_edge(dGi_Ti[0], dGi_Ti[1])
+        self.k2, self.kn2 = self.gen_k_edge(dGi_Ti[2], dGi_Ti[3]) #for HER, dG of the cycle is 0 vs NHE
+        self.k3, self.kn3 = self.gen_k_edge(self.dG-dGi_Ti[0]-dGi_Ti[2], dGi_Ti[4]) #for HER, dG of the cycle is 0 vs NHE
+
+    def func(self, VpH, dG1, T1, dG2, T2, T3):
+        self.genConsts((dG1, T1, dG2, T2, T3))
+        rate = self.rate(VpH[0,:], VpH[1,:])
+        return np.log10(rate)
+
+    def disorderFunc(self, VpH, dG1, T1, dG2, T2, T3, sig):
+        rate = 0.
+        mod_ddGs = self.ddGs*0.5*sig*2**0.5
+        for i in range(len(self.weights)):
+            self.genConsts((dG1+mod_ddGs[i], T1, T2))
+            rate += self.weights[i]*self.rate(VpH[0,:], VpH[1,:])
+        return np.log10(rate)
 
 pH_values = [1,2,3,4,6,7,8,9,10,11,12] #1-12 except 5
 pH_list = []
@@ -118,6 +143,7 @@ data = data.assign(pH=pH_list)
 #print(data[:29])
 #print(data[29:])
 data = data[data["pH"] != 4]
+data = data[data["pH"] > 4]
 
 fig, ax = plt.subplots()
 
@@ -131,11 +157,9 @@ weights *= 1./(np.pi**0.5) # the weights from gaussian quadrature actually sum t
 deltas *= 0.5*sig*2**0.5
 
 ### Plot of MJdata with 2 reversible PCET's (no disorder)
-#opt_mech = RevPcet2()
+opt_mech = RevPcet2()
 #popt, pcov = opt.curve_fit(opt_mech.func, x_data, y_data, p0=(-0.2,0.01,0.01), bounds=((-2.,0.,0.),(-0.0000001,np.inf,np.inf)))
-#print(popt, pcov)
 #mech_rate = opt_mech.func(x_data, popt[0], popt[1], popt[2])
-#plt.scatter(mech_rate, -1.*x_data[0,:], label='Fit: no disorder')
 
 ### Plot of MJdata with 2 reversible PCET's (disorder on first step)
 #opt_mech = RevPcet2(weights=weights, ddGs=deltas)
@@ -144,6 +168,10 @@ deltas *= 0.5*sig*2**0.5
 #plt.scatter(mech_rate, -1.*x_data[0,:], label='Fit: Disorder')
 #mech_rate_mf = opt_mech.func(x_data, popt[0], popt[1], popt[2])
 #plt.scatter(mech_rate_mf, -1.*x_data[0,:], label='NoDis w/ dis consts)')
+
+###Plot of MJdata with RevPcet2: disorder, variable total energy
+popt, pcov = opt.curve_fit(opt_mech.funcDisFullE, x_data, y_data, p0=(-0.2,0.01,0.01,1.2,0.0), bounds=((-5.0,0.,0.,0.,-5.0),(5.0,np.inf,np.inf,5.0,5.0)))
+mech_rate = opt_mech.funcDisFullE(x_data, popt[0], popt[1], popt[2], popt[3], popt[4]) #optimizing for disorder makes it real big
 
 ### Plot MJdata fit with PCET-ET (no disorder) --> the pH dependence on Pcet2 is too high, so this curbs it
 #opt_mech = RevPcetEt()
@@ -164,16 +192,22 @@ deltas *= 0.5*sig*2**0.5
 
 ### Plot MJdata with acid-base mechanism switch
 #opt_mech = RevPcetEt(weights=weights, ddGs=deltas, mech="both", dG=0.)
-#popt, pcov = opt.curve_fit(opt_mech.funcDisFullEAB, x_data, y_data, p0=(-0.2,0.01,0.01,0.4,0.0,5.0), \
+#popt, pcov = opt.curve_fit(opt_mech.funcDisFullEAB, x_data, y_data, p0=(-0.2,0.01,0.01,0.4,0.0,10.**-7), \
 #                           bounds=((-5.0,0.,0.,0.,-5.0,0.),(5.0,np.inf,np.inf,5.0,5.0,np.inf)))
 #mech_rate = opt_mech.funcDisFullEAB(x_data, popt[0], popt[1], popt[2], popt[3], popt[4], popt[5]) #optimizing for disorder makes it real big
 
 
 ### Plot MJdata with acid-base mechanism switch on Pcet2
-opt_mech = RevPcet2(weights=weights, ddGs=deltas, mech="both", dG=-0.01)
-popt, pcov = opt.curve_fit(opt_mech.funcDisFullEAB, x_data, y_data, p0=(-0.1,0.01,0.01,0.2,1.0,1.0), \
-                           bounds=((-5.0,0.,0.,0.,0.,0.),(5.0,np.inf,np.inf,5.0,np.inf,np.inf)))
-mech_rate = opt_mech.funcDisFullEAB(x_data, popt[0], popt[1], popt[2], popt[3], popt[4], popt[5]) #optimizing for disorder makes it real big
+#opt_mech = RevPcet2(weights=weights, ddGs=deltas, mech="both", dG=-0.01)
+#popt, pcov = opt.curve_fit(opt_mech.funcDisFullEAB, x_data, y_data, p0=(-0.1,0.01,0.01,0.2,10.**-7,10.**-7), \
+#                           bounds=((-5.0,0.,0.,0.,0.,0.),(5.0,np.inf,np.inf,5.0,np.inf,np.inf)))
+#mech_rate = opt_mech.funcDisFullEAB(x_data, popt[0], popt[1], popt[2], popt[3], popt[4], popt[5]) #optimizing for disorder makes it real big
+
+### Plot MJdata with PCET, ET, PT
+#opt_mech = RevPcet2(weights=weights, ddGs=deltas, mech="both", dG=-0.01)
+#popt, pcov = opt.curve_fit(opt_mech.funcDisFullEAB, x_data, y_data, p0=(-0.1,0.01,0.01,0.2,10.**-7,10.**-7), \
+#                           bounds=((-5.0,0.,0.,0.,0.,0.),(5.0,np.inf,np.inf,5.0,np.inf,np.inf)))
+#mech_rate = opt_mech.funcDisFullEAB(x_data, popt[0], popt[1], popt[2], popt[3], popt[4], popt[5]) #optimizing for disorder makes it real big
 
 print(popt) #, pcov)
 plt.scatter(mech_rate, -1.*x_data[0,:], label='Fit: Disorder')
@@ -213,8 +247,11 @@ plt.xlabel(r"log(J/Acm$^{-2}$)")
 #plt.savefig("MJdata_RevPcetEt_disorder.png", transparent=True, bbox_inches='tight', pad_inches=0.02)
 #plt.savefig("MJdata_RevPcetEt_FreeDisorder.png", transparent=True, bbox_inches='tight', pad_inches=0.02)
 #plt.savefig("MJdata_RevPcetEt_FreeDisorder_dGfixed.png", transparent=True, bbox_inches='tight', pad_inches=0.02)
+#plt.savefig("MJdata_RevPcetEt_FreeDisorder_dG.png", transparent=True, bbox_inches='tight', pad_inches=0.02)
+#plt.savefig("MJdata_RevPcetEt_FreeDisorder_dG_acid.png", transparent=True, bbox_inches='tight', pad_inches=0.02)
 #plt.savefig("MJdata_RevPcetEt_FreeDisorder_High.png", transparent=True, bbox_inches='tight', pad_inches=0.02)
 #plt.savefig("MJdata_RevPcetEt_FreeDisorder_AB.png", transparent=True, bbox_inches='tight', pad_inches=0.02)
+#plt.savefig("MJdata_RevPcet2_Disorder_AB.png", transparent=True, bbox_inches='tight', pad_inches=0.02)
 
 ## Plot of linear regression slopes as a function of pH
 #plt.scatter(pH_values, slope_list, s=100)
